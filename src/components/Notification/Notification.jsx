9fbf2946c5fbb54be1d4072642bc70d99da186d4
Raj <rajkapuriya03@gmail.com>
2023-05-11 13:36:11 +0530

    Added pagination in notification and fixed issue of my carts

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,5 +1,13 @@
 import React, { useState, useEffect, useContext } from 'react'
-import { Tabs, Tab, Box, Button, Grid, Typography } from '@mui/material'
+import {
+  Tabs,
+  Tab,
+  Box,
+  Button,
+  Grid,
+  Typography,
+  Pagination,
+} from '@mui/material'
 import './index.css'
 import AddRoundedIcon from '@mui/icons-material/AddRounded'
 import { GetNotification } from '../../services/apiservices/adminprofile'
@@ -19,7 +27,12 @@ const Notification = () => {
     useContext(ContextSnackbar)
   const [notificationSentDetail, setNotificationSentDetail] = useState([])
   const [notificationDetail, setNotificationDetail] = useState([])
+  const [numbersToDisplayOnPagination, setNumbersToDisplayOnPagination] =
+    useState(0)
+  const [rowsPerPage, setRowsPerPage] = useState(20)
 
+  const [totalResult, setTotalresult] = useState(null)
+  const [currentPage, setCurrentPage] = useState(1)
   const [AddNotificationDialog, setAddNotificationDialog] = useState(false)
   const [deleteRemainderDialog, setDeleteRemainderDialog] = useState(false)
   const [loader, setLoader] = useState(false)
@@ -46,10 +59,16 @@ const Notification = () => {
   useEffect(() => {
     setLoader(true)
     GetNotification(
-      {},
+      { page: currentPage, size: rowsPerPage },
       res => {
         if (res?.success) {
-          setNotificationDetail(res?.data)
+          setTotalresult(res?.data?.totalPage)
+          let pages =
+            res?.data?.totalPage > 0
+              ? Math.ceil(res?.data?.totalPage / rowsPerPage)
+              : null
+          setNumbersToDisplayOnPagination(pages)
+          setNotificationDetail(res?.data.notifications)
           setLoader(false)
         }
       },
@@ -87,93 +106,106 @@ const Notification = () => {
     <>
       <Box className="main_section" sx={{ padding: '0px', overflowY: 'auto' }}>
         <Box sx={{ padding: '10px' }}>
-          {notificationDetail.map((rowData, index) => {
-            return (
-              <>
-                <Box sx={{ marginBottom: '20px' }}>
-                  {moment(rowData?.createdAt).format('DD-MM-YYYY') ===
-                  moment(notificationDetail[index - 1]?.createdAt).format(
-                    'DD-MM-YYYY',
-                  ) ? null : (
-                    <Typography>
-                      {moment(rowData.createdAt).format('DD-MM-YYYY')}
-                    </Typography>
-                  )}
-
-                  <Box className="notification_content">
-                    <Box
-                      sx={{
-                        width: '6%',
-                      }}
-                    >
-                      {
-                        <img
-                          style={{ height: '30px', width: '30px' }}
-                          src={`${
-                            window.location.protocol +
-                            '//' +
-                            window.location.hostname +
-                            ':' +
-                            window.location.port
-                          }/${rowData.type.toLowerCase()}.svg`}
-                          alt="mySvgImage"
-                        />
-                      }
-                    </Box>
-
-                    <Box
-                      sx={{
-                        width: '10%',
-                      }}
-                    >
-                      <Typography variant="span">
-                        {moment(
-                          moment(rowData.createdAt).format(
-                            'YYYY-MM-DD HH:mm:ss',
-                          ),
-                        ).format('LT')}
+          {notificationDetail.length > 0 &&
+            notificationDetail.map((rowData, index) => {
+              return (
+                <>
+                  <Box sx={{ marginBottom: '20px' }}>
+                    {moment(rowData?.createdAt).format('DD-MM-YYYY') ===
+                    moment(notificationDetail[index - 1]?.createdAt).format(
+                      'DD-MM-YYYY',
+                    ) ? null : (
+                      <Typography>
+                        {moment(rowData.createdAt).format('DD-MM-YYYY')}
                       </Typography>
-                    </Box>
+                    )}
 
-                    <Box
-                      sx={{
-                        width: '84%',
-                        display: 'flex',
-                        flexDirection: 'column',
-                      }}
-                    >
-                      <Typography
-                        className="notification_description_heading"
-                        variant="span"
+                    <Box className="notification_content">
+                      <Box
+                        sx={{
+                          width: '6%',
+                        }}
                       >
-                        {rowData.heading}
-                      </Typography>
-                      <Typography variant="span">
-                        {rowData.description}
-                      </Typography>
-                    </Box>
+                        {
+                          <img
+                            style={{ height: '30px', width: '30px' }}
+                            src={`${
+                              window.location.protocol +
+                              '//' +
+                              window.location.hostname +
+                              ':' +
+                              window.location.port
+                            }/${rowData.type.toLowerCase()}.svg`}
+                            alt="mySvgImage"
+                          />
+                        }
+                      </Box>
 
-                    <Box>
-                      {rowData?.button &&
-                        JSON.parse(rowData?.button).map(value => {
-                          return (
-                            <Button
-                              sx={{ backgroundColor: '#2E3591' }}
-                              className="notification_button"
-                              variant="contained"
-                              onClick={() => handleView(value?.path)}
-                            >
-                              {value?.name}
-                            </Button>
-                          )
-                        })}
+                      <Box
+                        sx={{
+                          width: '10%',
+                        }}
+                      >
+                        <Typography variant="span">
+                          {moment(
+                            moment(rowData.createdAt).format(
+                              'YYYY-MM-DD HH:mm:ss',
+                            ),
+                          ).format('LT')}
+                        </Typography>
+                      </Box>
+
+                      <Box
+                        sx={{
+                          width: '84%',
+                          display: 'flex',
+                          flexDirection: 'column',
+                        }}
+                      >
+                        <Typography
+                          className="notification_description_heading"
+                          variant="span"
+                        >
+                          {rowData.heading}
+                        </Typography>
+                        <Typography variant="span">
+                          {rowData.description}
+                        </Typography>
+                      </Box>
+
+                      <Box>
+                        {rowData?.button &&
+                          JSON.parse(rowData?.button).map(value => {
+                            return (
+                              <Button
+                                sx={{ backgroundColor: '#2E3591' }}
+                                className="notification_button"
+                                variant="contained"
+                                onClick={() => handleView(value?.path)}
+                              >
+                                {value?.name}
+                              </Button>
+                            )
+                          })}
+                      </Box>
                     </Box>
                   </Box>
-                </Box>
-              </>
-            )
-          })}
+                </>
+              )
+            })}
         </Box>
+        <Pagination
+          className="pagination_style"
+          boundaryCount={0}
+          siblingCount={0}
+          size="small"
+          shape="rounded"
+          count={numbersToDisplayOnPagination}
+          page={currentPage}
+          onChange={(e, value) => {
+            setCurrentPage(value)
+          }}
+        />
         {/* <ClientStatusCloseDialog
           deleteRemainderDialog={deleteRemainderDialog}
           CloseDeleteRemainder={CloseDeleteRemainder}
